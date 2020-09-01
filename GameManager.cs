b05c2774b0c12bf24620ed40159ecfff2f783d73
Red-Dawn24 <68660439+Red-Dawn24@users.noreply.github.com>
2020-09-01 12:16:13 -0700

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+    public GUISkin layout;
+    GameObject theBall;
+     void Start()
+    {
+        theBall = GameObject.FindGameObjectWithTag("Ball");
+    }
+    public static void Score (string wallID)
+    {
+        if(wallID == "Right Wall")
+        {
+            PlayerScore1++;
+        }
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+    private void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+    }
+}
